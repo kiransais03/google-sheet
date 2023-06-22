862225a9c0754d3759564b5eb658616286dc0bbd
Prikshit <prikshit.chawla@walmart.com>
2023-06-22 21:19:49 +0530

    update Matrix

--- script.js
+++ script.js
@@ -49,6 +49,20 @@ for (let row = 0; row < rows; row++) {
   // []
 // ]
 
+// updateMatrix will take currentCell
+function updateMatrix(currentCell){
+  let obj = {
+    style: currentCell.style.cssText,
+    text: currentCell.innerText,
+    id: currentCell.id,
+  }
+  // id -> B1, B2, D4
+  let id= currentCell.id.split('');
+  // id = [1-1,'A'-> number -> - 65] -> 0,0
+  let i=id[1]-1; ////1,2,3,4 /// 0,1,2,3
+  let j=id[0].charCodeAt(0)-65; /// A,B,C,D // 65,66,67,68 // 0,1,2,3
+  matrix[i][j]=obj;
+}
 
 for (let col = 0; col < columns; col++) {
     let th = document.createElement('th');
@@ -71,6 +85,7 @@ for(let row=1;row<=rows;row++){
         let td= document.createElement('td');
         td.setAttribute('contenteditable','true');
         td.setAttribute('id',`${String.fromCharCode(col+65)}${row}`)
+        td.addEventListener('input',(event)=>onInputFn(event));
         // this event listener will triger when any cell comes in focus;
         td.addEventListener('focus',(event)=> onFocusFn(event));
         tr.append(td);
@@ -78,11 +93,18 @@ for(let row=1;row<=rows;row++){
     tbody.append(tr);
 }
 
+function onInputFn(event){
+  updateMatrix(event.target);
+  // console.log(matrix); // uncomment to see updated in console
+}
+
 // BOLD BUTTON
 boldButton.addEventListener("click", () => {
   if (currentCell.style.fontWeight === "bold") {
     currentCell.style.fontWeight = "normal";
   } else currentCell.style.fontWeight = "bold";
+
+  updateMatrix(currentCell);
 });
 
 // ITALICS
@@ -91,6 +113,7 @@ italicsButton.addEventListener('click',()=>{
         currentCell.style.fontStyle = 'normal';
     }
     else currentCell.style.fontStyle = 'italic';
+    updateMatrix(currentCell);
 })
 
 // underline
@@ -98,27 +121,34 @@ underlineButton.addEventListener("click", () => {
   if (currentCell.style.textDecoration === "underline") {
     currentCell.style.textDecoration = "none";
   } else currentCell.style.textDecoration = "underline";
+  updateMatrix(currentCell);
 });
 
 // Left align button
 leftAlign.addEventListener('click',()=>{
     currentCell.style.textAlign='left';
+    updateMatrix(currentCell);
+
 })
 // Center align button
 centerAlign.addEventListener('click',()=>{
     currentCell.style.textAlign='center';
+    updateMatrix(currentCell);
 })
 // Right align button
 rightAlign.addEventListener('click',()=>{
     currentCell.style.textAlign='right';
+    updateMatrix(currentCell); // O(1)
 })
 // font Size
 fontSizeDropDown.addEventListener('change',()=>{
     currentCell.style.fontSize=fontSizeDropDown.value;
+    updateMatrix(currentCell);
 })
 // Font Family
 fontFamilyDropDown.addEventListener('change',()=>{
     currentCell.style.fontFamily=fontFamilyDropDown.value;
+    updateMatrix(currentCell);
 })
 
 cutButton.addEventListener("click", () => {
@@ -128,6 +158,8 @@ cutButton.addEventListener("click", () => {
   };
   currentCell.innerText = "";
   currentCell.style = null;
+  updateMatrix(currentCell);
+  // console.log(matrix);
 });
 
 copyButton.addEventListener("click", () => {
@@ -141,22 +173,24 @@ pasteButton.addEventListener("click", () => {
   if (cutCell.text) {
     currentCell.style = cutCell.style;
     currentCell.innerText = cutCell.text;
+    updateMatrix(currentCell);
   }
 });
 
 // using input
 bgColorInput.addEventListener("input", () => {
   currentCell.style.backgroundColor = bgColorInput.value;
+  updateMatrix(currentCell);
 });
 
 // using change
 textColorInput.addEventListener('change',()=>{
   currentCell.style.color = textColorInput.value;
+  updateMatrix(currentCell);
 });
 
 
 function onFocusFn(event){
-    // console.log(event);
     currentCell=event.target;
     document.getElementById('current-cell').innerText=currentCell.id;
 }
@@ -190,3 +224,35 @@ function onFocusFn(event){
 // 2dMatrix[1][0]
 // 2dMatrix[row][col]
 // rows-> number of arrays inside my main array
+
+
+// There are 2 ways of cloning my table
+
+// 1st we iterate over whole table and copy every cell
+// 2nd when we are editing any cell, we update that respective cell in 2d matrix
+
+  // [
+  //  [{},{},{}],
+  //  [{},{},{}],
+  //  [{},{},{}],
+  // ]
+
+// user invokes download function
+  // row * col
+  // [
+  //    [{},{},{}],
+  //    [{},{},{}],
+  //    [{},{},{}],
+  // ]
+
+// number of cells edited
+  // [
+  //  [{},{},{}],
+  //  [{},{},{}],
+  //  [{},{},{}],
+  // ]
+
+  // A1 -> row -> 1 -1
+  // A -> 0
+
+  // matrix[(row-1)(letter to number -65)]
